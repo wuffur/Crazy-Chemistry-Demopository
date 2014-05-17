869080d7e4cbafc413c8e61791a89d602b703f85
Ivan Filippov <wuffur@gmail.com>
2014-05-17 18:12:14 +0400

    Create interfifo.c
    This is unknown BUG!

---
+++ interfifo.c
@@ -0,0 +1,10 @@
+#include<iostream.h> /* deprecated since C00x24 */
+#include<struct.h>
+
+int main()
+{
+  printf(); /* let standart output flush */
+  std::cout << "The sun is shinin'!\tThe weather is good!\n";
+  std::cout << "I`m da rainbow now {)!\n";
+  return ctoi("");
+}
